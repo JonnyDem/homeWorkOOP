29b8beb2dd7bffb6276fbc838f6cc3c0a38ae327
JonnyDem <tl-k@ukr.net>
2017-08-27 15:17:17 +0300

    Conditioners(27.08.17)

---
+++ src/homeWorkOOP/conditioner/BlockConditioners.java
@@ -0,0 +1,79 @@
+package homeWorkOOP.conditioner;
+
+/*
+Тема : Делегирование
+Задача 3: (стр 45-46)
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BlockConditioners {
+
+    static ArrayList<Conditioner> conditioners = new ArrayList<>();
+
+     BlockConditioners(){
+        for (int i = 0; i <10 ; i++) {
+            conditioners.add(i, new Conditioner());
+        }
+    }
+
+
+    public class Conditioner{
+       private int temperature;
+       private boolean turnOn;
+       private boolean getTurn;
+
+        public int setTemperature() {
+            Scanner scan=new Scanner(System.in);
+            System.out.println("Select temperature from 5 to 27");
+            int temperature=scan.nextInt();
+            if(temperature>=5&&temperature<=27) {
+                this.temperature = temperature;
+                System.out.println("The temperature is set on "+temperature+" degrees.");
+                return temperature;
+            } else {
+                return setTemperature();
+            }
+        }
+
+        public int getTemperature() {
+            return temperature;
+        }
+
+        public void turnOn(){
+            if(turnOn){
+                System.out.println("This conditioner has already been turned on.");
+            } else {
+                turnOn = true;
+                getTurn();
+                System.out.println("This conditioner is turned on.");
+            }
+
+        }
+        public void turnOf(){
+            if(!turnOn){
+                System.out.println("This conditioner has already been turned off.");
+            } else {
+               this.turnOn = false;
+                getTurn();
+                System.out.println("This conditioner is turned off.");
+            }
+
+        }
+        public void getTurn(){
+        if(turnOn){
+            getTurn = true;
+        } else {
+            getTurn = false;
+        }
+        }
+
+        public boolean isGetTurn() {
+            return getTurn;
+        }
+        public boolean isTurnOn() {
+            return turnOn;
+        }
+    }
+}
---
+++ src/homeWorkOOP/conditioner/MainConditioner.java
@@ -0,0 +1,61 @@
+package homeWorkOOP.conditioner;
+/*
+Тема : Делегирование
+Задача 3: (стр 45-46)
+ */
+
+import java.util.Scanner;
+
+public class MainConditioner {
+   static String [] menuList={"1.Show condition of all conditioners","2.Turn on conditioner(index)","3.Turn off conditioner(index)","4.Set temperature to conditioner(index)","5.Exit"};
+
+    public static void main(String[] args) {
+        BlockConditioners blockConditioners = new BlockConditioners();
+        boolean runWhile=true;
+        Scanner scan=new Scanner(System.in);
+        while(runWhile) {
+            System.out.println("Select operation");
+            for(String s:menuList){
+                System.out.println(s);
+            }
+            int operation=scan.nextInt();
+            switch (operation){
+                case 1:
+                    int count=0;
+                    for (BlockConditioners.Conditioner c:BlockConditioners.conditioners) {
+                        System.out.print("Conditioner "+count+": is turned on: "+ c.isTurnOn()+", ");
+                        System.out.print("Is opened(turned): "+c.isGetTurn()+", ");
+                        System.out.println("temperature: "+c.getTemperature()+", ");
+                        count++;
+                    }
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("Select conditioner you want to turn on.");
+                    int condTurnOn=scan.nextInt();
+                    BlockConditioners.conditioners.get(condTurnOn).turnOn();
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("Select conditioner you want to turn off.");
+                    int condTurnOff=scan.nextInt();
+                    BlockConditioners.conditioners.get(condTurnOff).turnOf();
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.println("Select conditioner you want to set the temperature.");
+                    int condSetTemperature=scan.nextInt();
+                    BlockConditioners.conditioners.get(condSetTemperature).setTemperature();
+                    System.out.println();
+                    break;
+                case 5:
+                    runWhile = false;
+                    break;
+               default:
+                   System.out.println("Select from 1 to 5");
+                   System.out.println();
+                   break;
+            }
+        }
+    }
+}
